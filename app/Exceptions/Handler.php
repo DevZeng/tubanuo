9edca056e8df3a80f9ab84316b9ed3b672036d8a
DevZeng <zengshungeng@live.com>
2020-04-07 11:33:30 +0800

    Update Handler.php

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -51,6 +51,6 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-        parent::render($exception);
+        return parent::render($request, $exception);
     }
 }
