ba5c02704b2680d222f726684f386856cd7ec832
h272101298 <272101298@qq.com>
2020-04-09 17:45:23 +0800

    312

--- app/Console/Commands/Notice.php
+++ app/Console/Commands/Notice.php
@@ -46,7 +46,7 @@ class Notice extends Command
         $student=Student::where('stu_status','=',1)->orWhere('stu_status','=',2)->get();
         $template="sExfJnV0OMkzfcSNSNxpBbl6DrLO2VLXfIPCejmM1lM";
         foreach ($student as $value){
-            $count=StudentStatus::where('student_id',$value->stu_id)->where('stu_status',1)->count();
+            $count=StudentStatus::where('student_id',$value->stu_id)->count();
             if ($count == 0){
                 $data=[
                     'touser'=>$value->user_openid,
