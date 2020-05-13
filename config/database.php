0def6afabd31c8f46e9cdd1fbf2059ee73cb17c0
ZENG <zeng@ZENGdeiMac.local>
2020-05-13 12:17:28 +0800

    fix

--- config/database.php
+++ config/database.php
@@ -105,7 +105,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE_SHAXI', 'forge'),
+            'database' => env('DB_DATABASE_XIJIAO', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
