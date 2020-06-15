415f1e18774bfb4095da5611f43e07e8586d385d
h272101298 <272101298@qq.com>
2020-06-15 12:19:21 +0800

    config

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -426,7 +426,7 @@ class UserController extends Controller
     public function getConfig(Request $post){
         $version = $post->get('version');
         $school = $post->get('school');
-        if ($school=='all'&&$version==2000){
+        if ($school=='all'&&$version==2001){
            $config = [
                'key'=>'open',
                 'value'=>0
