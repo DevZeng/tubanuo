5b68e682c4aa93540e2646a5f0ec22d279fc4840
h272101298 <272101298@qq.com>
2020-09-03 10:31:18 +0800

    fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -446,7 +446,7 @@ class UserController extends Controller
                 'data'=>$config
             ]);
         }
-        if ($school=='all'&&$version=='2007'){
+        if ($school=='all'&&$version=='2008'){
             $config = [
                 'key'=>'open',
                 'value'=>0
